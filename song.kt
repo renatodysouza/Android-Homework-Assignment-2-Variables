8487a4534f604c08b4bab45a086a29bc13987df5
serra <renatoserra@gmail.com>
2019-06-12 23:04:55 +0100

    done

---
+++ song.kt
@@ -0,0 +1,27 @@
+
+
+/*
+  Create variable string
+ */  
+var artist: String = "elvis"
+
+/*
+  Create variable string
+ */ 
+var song: String = "suspicious minds"
+
+/*
+  Create variable string
+ */ 
+var year: String = "1969"
+
+/*
+  Create variable integer
+ */ 
+var deathAge:Int = 42
+
+/*
+  Create variable boolean
+ */ 
+var death:Boolean = true
+
